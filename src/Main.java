9d9e46b721622d2556a087faacf1a076261ec337
Pavel <pavellgindemit@mail.ru>
2023-12-15 17:49:21 +0400

    commit number 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Gindemit Pavel 5 lab GIT");
+        //COMMIT 2
+        System.out.println("Commit test number 2");
+        int x = 5;
+        System.out.println(x);
     }
 }
